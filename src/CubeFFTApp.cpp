dcb61e9c020ac804e52fa39b6a15254c740f0292
acanois <david.richter42@gmail.com>
2019-09-15 19:54:58 -0500

    audio input works! No thanks to Apple

--- src/CubeFFTApp.cpp
+++ src/CubeFFTApp.cpp
@@ -1,10 +1,10 @@
 #include "cinder/app/App.h"
 #include "cinder/app/RendererGl.h"
 #include "cinder/gl/gl.h"
+#include "cinder/audio/audio.h"
 
 using namespace ci;
 using namespace ci::app;
-using namespace std;
 
 class CubeFFTApp : public App {
   public:
@@ -15,6 +15,15 @@ class CubeFFTApp : public App {
     
     static void prepareSettings( Settings* settings );
     
+    void printFFTInfo();
+    
+private:
+    // Audio
+    audio::InputDeviceNodeRef        mInputDeviceNode;
+    audio::MonitorSpectralNodeRef    mMonitorSpectralNode;
+    std::vector<float>               mMagSpectrum;
+    
+    // Visual
     CameraPersp  mCam;
     gl::BatchRef mCubeBatch;
 };
@@ -28,6 +37,23 @@ void CubeFFTApp::prepareSettings( Settings* settings )
 
 void CubeFFTApp::setup()
 {
+    //============CUBE CLASS====================
+    auto ctx = audio::Context::master();
+    
+    mInputDeviceNode = ctx->createInputDeviceNode();
+    
+    // Use double FFT size vs windowSize because it causes the FFT to be zero-padded
+    auto monitorFormat = audio::MonitorSpectralNode::Format().fftSize( 32 ).windowSize( 16 );
+    mMonitorSpectralNode = ctx->makeNode( new audio::MonitorSpectralNode( monitorFormat ) );
+
+    mInputDeviceNode->connect( mMonitorSpectralNode );
+    
+    mInputDeviceNode->enable();
+    ctx->enable();
+    
+    getWindow()->setTitle( mInputDeviceNode->getDevice()->getName() );
+    //==========================================
+    
     //============CUBE CLASS====================
     auto color = gl::ShaderDef().color();
     gl::GlslProgRef shader = gl::getStockShader( color );
@@ -45,6 +71,16 @@ void CubeFFTApp::setup()
     mCam.lookAt( eyePoint, target );
     //==========================================
     
+    printFFTInfo();
+}
+
+void CubeFFTApp::printFFTInfo()
+{
+    size_t numBins = mMonitorSpectralNode->getNumBins();
+    float nyquist = static_cast<float>( audio::master()->getSampleRate() / 2.f );
+    
+    console() << "Bin Count: " << numBins << std::endl;
+    console() << "Nyquist:   " << nyquist << std::endl;
 }
 
 void CubeFFTApp::mouseDown( MouseEvent event )
@@ -53,10 +89,16 @@ void CubeFFTApp::mouseDown( MouseEvent event )
 
 void CubeFFTApp::update()
 {
+//    mMagSpectrum = mMonitorSpectralNode->getMagSpectrum();
 }
 
 void CubeFFTApp::draw()
 {
+    mMagSpectrum = mMonitorSpectralNode->getMagSpectrum();
+    float mag = audio::linearToDecibel( mMagSpectrum[1] );
+    float freq = mMonitorSpectralNode->getFreqForBin( 1 );
+    console() << mag << std::endl;
+    
 	gl::clear( Color( 0, 0, 0 ) );
     
     //============CUBE CLASS====================
@@ -64,14 +106,17 @@ void CubeFFTApp::draw()
     gl::enableDepthWrite();
     gl::setMatrices( mCam );
     
-    size_t numCubes = 10;
+    size_t numCubes = 16;
+    
+//    for (auto level : mMagSpectrum)
+//        console() << level << std::endl;
     
     for ( size_t i = 0; i < numCubes; ++i )
     {
         gl::pushModelMatrix();
         gl::translate( vec3( 0.f, 0.f, 0.f ) );
         gl::color( ColorA( 1.f, 1.f, 1.f, 1.f ) );
-        gl::scale( vec3( i * 0.1f ) );
+        gl::scale( vec3( i * 0.2f ) );
         mCubeBatch->draw();
         gl::popModelMatrix();
     }
