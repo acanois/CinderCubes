1a69fab2e6bc3cfe0aaa0999ec5fad53c4a24d82
acanois <david.richter42@gmail.com>
2019-09-17 16:18:57 -0500

    it's purple now

--- src/CubeFFTApp.cpp
+++ src/CubeFFTApp.cpp
@@ -15,7 +15,7 @@ using namespace ci;
 using namespace ci::app;
 
 class CubeFFTApp : public App {
-  public:
+public:
 	void setup() override;
 	void mouseDown( MouseEvent event ) override;
 	void update() override;
@@ -50,30 +50,25 @@ void CubeFFTApp::prepareSettings( Settings* settings )
 
 void CubeFFTApp::setup()
 {
-    //============AUDIO CLASS====================
+    //=================AUDIO====================
     auto ctx = audio::Context::master();
     
     mInputDeviceNode = ctx->createInputDeviceNode();
     
-    // Use double FFT size vs windowSize because it causes the FFT to be zero-padded
     auto monitorFormat = audio::MonitorSpectralNode::Format().fftSize( 64 ).windowSize( mWindowSize );
-    mMonitorSpectralNode = ctx->makeNode( new audio::MonitorSpectralNode( monitorFormat ) );
-
-    mInputDeviceNode->connect( mMonitorSpectralNode );
     
+    mMonitorSpectralNode = ctx->makeNode( new audio::MonitorSpectralNode( monitorFormat ) );
+    mInputDeviceNode->connect( mMonitorSpectralNode );
     mInputDeviceNode->enable();
     ctx->enable();
-    
     getWindow()->setTitle( mInputDeviceNode->getDevice()->getName() );
-    //==========================================
     
-    //============CUBE CLASS====================
+    //==================CUBE====================
     auto color = gl::ShaderDef().color();
     gl::GlslProgRef shader = gl::getStockShader( color );
-    mCubeBatch = gl::Batch::create( geom::WireCube( vec3( 1.f ), ivec3( 1 ) ), shader );
-    //==========================================
+    mCubeBatch = gl::Batch::create( geom::WireCube( vec3( 1.f, 1.f, 1.f ), ivec3( 0 ) ), shader );
     
-    //=============CAMERA CLASS=================
+    //=================CAMERA===================
     auto fov = 60;
     auto nearPlane = 1;
     auto farPlane = 1000;
@@ -82,9 +77,6 @@ void CubeFFTApp::setup()
     
     mCam.setPerspective( fov, getWindowAspectRatio(), nearPlane, farPlane );
     mCam.lookAt( eyePoint, target );
-    //==========================================
-    
-//    printFFTInfo();
 }
 
 void CubeFFTApp::printFFTInfo()
@@ -103,38 +95,37 @@ void CubeFFTApp::mouseDown( MouseEvent event )
 void CubeFFTApp::update()
 {
     mMagSpectrum = mMonitorSpectralNode->getMagSpectrum();
-    mTheta += 0.001;
+    mTheta += 0.002;
 }
 
 void CubeFFTApp::draw()
 {
-	gl::clear( Color( 0, 0, 0 ) );
+	gl::clear( Color( 0.2f, 0, 0.2f ) );
     
-    //============CUBE CLASS====================
     gl::enableDepthRead();
     gl::enableDepthWrite();
     gl::enableAdditiveBlending();
-    gl::enableAlphaBlending();
     gl::setMatrices( mCam );
     
     for ( size_t i = 0; i < mWindowSize; ++i )
     {
         // Close enough for now
-        float alpha = mMagSpectrum[i];
+        float alpha = audio::linearToDecibel( mMagSpectrum[i] ) * 0.01;
         float hue = i / static_cast<float>( mWindowSize );
-        
-        console() << "hue " << i << ": " << hue << std::endl;
+        float freq = mMonitorSpectralNode->getFreqForBin( i ) * 2;
         
         gl::pushModelMatrix();
-        gl::translate( vec3( 0.f, 0.f, 0.f ) );
-        gl::color( Color( CM_HSV, hue, 1.f, alpha ) );
-        gl::scale( vec3( i * 0.2f ) );
+        gl::translate( vec3( std::sin( mTheta ), 0.f, std::cos( mTheta ) ) );
+        
+        gl::rotate( i * ( ( std::sin( mTheta ) ) * 0.1f ), vec3( 1.f, 0.f, 0.f ) );
+        gl::rotate( mTheta, vec3( std::sin( mTheta ), 0.f, std::cos( 1.f ) ) );
+        
+        gl::color( Color( CM_HSV, hue, 0.f, alpha ) );
+        gl::scale( vec3( i * 0.3f ) );
         mCubeBatch->draw();
         gl::popModelMatrix();
     }
-    //==========================================
     
-    gl::disableAlphaBlending();
     gl::disableDepthWrite();
     gl::disableDepthRead();
 }
